44db344e7b1aabb3e9390d027b08fd5e6ba23490
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-05-08 20:59:29 -0500

    feature : add type converter to the date

--- app/src/main/java/com/kalex/bookyouu_notesapp/db/BookYouuDataBase.kt
+++ app/src/main/java/com/kalex/bookyouu_notesapp/db/BookYouuDataBase.kt
@@ -2,15 +2,18 @@ package com.kalex.bookyouu_notesapp.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.kalex.bookyouu_notesapp.data.Note
-import com.kalex.bookyouu_notesapp.data.Subject
+import androidx.room.TypeConverters
 import com.kalex.bookyouu_notesapp.db.dao.NoteDao
 import com.kalex.bookyouu_notesapp.db.dao.SubjectDao
+import com.kalex.bookyouu_notesapp.db.data.Note
+import com.kalex.bookyouu_notesapp.db.data.Subject
+import com.kalex.bookyouu_notesapp.db.typeConvertes.DateTypeConverter
 
 @Database(
     entities = [Subject::class, Note::class],
     version = 1
 )
+@TypeConverters(DateTypeConverter::class)
 abstract class BookYouuDataBase : RoomDatabase() {
     abstract val subjectDao: SubjectDao
     abstract val noteDao: NoteDao

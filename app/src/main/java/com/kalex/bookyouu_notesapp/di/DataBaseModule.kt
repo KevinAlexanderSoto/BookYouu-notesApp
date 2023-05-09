a64da9f069ce0f8a6f2c6957d65877d205dca4cf
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-05-08 21:33:50 -0500

    fix : add data base module

---
+++ app/src/main/java/com/kalex/bookyouu_notesapp/di/DataBaseModule.kt
@@ -0,0 +1,31 @@
+package com.kalex.bookyouu_notesapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.kalex.bookyouu_notesapp.db.BookYouuDataBase
+import com.kalex.bookyouu_notesapp.db.dao.NoteDao
+import com.kalex.bookyouu_notesapp.db.dao.SubjectDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DataBaseModule {
+    @Provides
+    fun provideBookYouuDataBase(@ApplicationContext context: Context): BookYouuDataBase {
+        return Room.databaseBuilder(context, BookYouuDataBase::class.java, "BookYouuDataBase").build()
+    }
+
+    @Provides
+    fun provideNoteDao(database: BookYouuDataBase): NoteDao {
+        return database.noteDao
+    }
+
+    @Provides
+    fun provideSubjectDao(database: BookYouuDataBase): SubjectDao {
+        return database.subjectDao
+    }
+}

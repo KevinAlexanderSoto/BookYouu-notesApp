32944bcf86250ecf3215c2be33123d16ae6565d1
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-05-14 05:18:41 -0500

    feature : add checkbox and input text composables

---
+++ app/src/main/java/com/kalex/bookyouu_notesapp/ui/composables/OutLineTextInputField.kt
@@ -0,0 +1,25 @@
+package com.kalex.bookyouu_notesapp.ui.composables
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OutLineTextInputField(
+    label: String,
+) {
+    var text by remember { mutableStateOf("") }
+    OutlinedTextField(
+        value = text,
+        label = { Text(text = label) },
+        onValueChange = {
+            text = it
+        },
+    )
+}

795a13e6d7eef140285dbba58b92b2075ff0c0a6
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-06-11 23:12:50 -0500

    refactor: use generic and create func to handle viewmodel states

--- app/src/main/java/com/kalex/bookyouu_notesapp/subject/SubjectMainScreen.kt
+++ app/src/main/java/com/kalex/bookyouu_notesapp/subject/SubjectMainScreen.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kalex.bookyouu_notesapp.R
 import com.kalex.bookyouu_notesapp.common.composables.BYLoadingIndicator
 import com.kalex.bookyouu_notesapp.common.composables.EmptyScreen
-import com.kalex.bookyouu_notesapp.common.ViewModelState
+import com.kalex.bookyouu_notesapp.common.handleViewModelState
 import com.kalex.bookyouu_notesapp.subject.subjectList.presentation.SubjectListViewModel
 import com.kalex.bookyouu_notesapp.subject.subjectList.presentation.ui.SubjectListScreen
 
@@ -21,26 +21,26 @@ fun SubjectMainScreen(
         subjectViewModel.getSubjectList()
     }
 
-    when (val response = subjectViewModel.getSubjectState.collectAsStateWithLifecycle().value) {
-        is ViewModelState.Empty -> {
+    handleViewModelState(
+        subjectViewModel.getSubjectState.collectAsStateWithLifecycle(),
+        onEmpty = {
             EmptyScreen(
                 onAddItemClick = { onAddNewSubject.invoke() },
                 rationaleText = R.string.subjectList_no_subjectsFount_text,
             )
-        }
-
-        is ViewModelState.Error -> TODO()
-        is ViewModelState.Loading -> BYLoadingIndicator()
-        is ViewModelState.Success -> {
+        },
+        onLoading = { BYLoadingIndicator() },
+        onSuccess = { response ->
             SubjectListScreen(
-                response.data,
+                response,
                 onSubjectClickAction = {
                     onSubjectClickAction(it.toString())
                 },
                 onAddSubjectClickAction = { onAddNewSubject.invoke() },
             )
-        }
-
-        else -> {}
-    }
+        },
+        onError = {
+            TODO()
+        },
+    )
 }

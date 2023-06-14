b7ea9c712720b92e0375aad6324c7a290e359b7b
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-06-13 23:47:57 -0500

    fix: permission logic

--- app/src/main/java/com/kalex/bookyouu_notesapp/permission/RequireCameraPermission.kt
+++ app/src/main/java/com/kalex/bookyouu_notesapp/permission/RequireCameraPermission.kt
@@ -18,18 +18,18 @@ fun RequireCameraPermission(
         content()
     } else {
         if (multiplePermissionsState.shouldShowRationale) {
-            RationaleDialog(
-                contentText = stringResource(id = R.string.permission_rationale_content_text),
-                titleText = stringResource(id = R.string.permission_rationale_title_text),
-                buttonText = stringResource(id = R.string.permission_rationale_button_text),
-                onRequestPermission = { multiplePermissionsState.launchMultiplePermissionRequest() },
-            )
-        } else {
             PermissionNotAvailable(
                 titleText = stringResource(id = R.string.permission_notAvailable_title_text),
                 contentText = stringResource(id = R.string.permission_notAvailable_content_text),
                 buttonText = stringResource(id = R.string.permission_notAvailable_button_text),
                 onConfirmClick = { onPermissionDenied() },
+            )
+        } else {
+            RationaleDialog(
+                contentText = stringResource(id = R.string.permission_rationale_content_text),
+                titleText = stringResource(id = R.string.permission_rationale_title_text),
+                buttonText = stringResource(id = R.string.permission_rationale_button_text),
+                onRequestPermission = { multiplePermissionsState.launchMultiplePermissionRequest() },
             )
         }
     }

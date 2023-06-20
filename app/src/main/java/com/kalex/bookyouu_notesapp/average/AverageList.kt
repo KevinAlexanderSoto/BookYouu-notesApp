6ef4c670daaeef31b063d16a12b353c4cc0cbcc0
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-06-20 18:37:47 -0500

    fix: change the color and imports for some labels and icons

--- app/src/main/java/com/kalex/bookyouu_notesapp/average/AverageList.kt
+++ app/src/main/java/com/kalex/bookyouu_notesapp/average/AverageList.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -73,7 +74,7 @@ fun AverageList(
                 Icon(
                     painterResource(id = R.drawable.remove_24px),
                     contentDescription = "add new",
-                    tint = Color.Black,
+                    tint = MaterialTheme.colorScheme.onBackground,
                 )
             }
             Text(

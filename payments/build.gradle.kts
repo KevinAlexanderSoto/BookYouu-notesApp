85437b214e7346b670203080ca8d9ff46203ee45
Kevin A. <kevinalexandersoto999@gmail.com>
2026-04-02 14:45:28 -0500

    feature: create base module Payments.

---
+++ payments/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    alias(libs.plugins.com.android.library)
+}
+
+android {
+    namespace = "com.kalex.bookyouu_notesapp.payments"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}

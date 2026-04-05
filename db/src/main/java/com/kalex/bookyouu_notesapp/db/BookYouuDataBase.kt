188a4bc456dc08efd0ae34b93fda4e63e8f1a138
Kevin A. <kevinalexandersoto999@gmail.com>
2026-04-04 20:55:24 -0500

    refactor: reset database version to 1 and remove schema history

--- db/src/main/java/com/kalex/bookyouu_notesapp/db/BookYouuDataBase.kt
+++ db/src/main/java/com/kalex/bookyouu_notesapp/db/BookYouuDataBase.kt
@@ -15,22 +15,8 @@ import com.kalex.bookyouu_notesapp.db.typeConvertes.DayOfWeekTypeConverter
 
 @Database(
     entities = [Subject::class, Note::class, ObligationEntity::class],
-    version = 4,
-    exportSchema = true,
-    autoMigrations = [
-        AutoMigration(
-            from = 1,
-            to = 2,
-        ),
-        AutoMigration(
-            from = 2,
-            to = 3,
-        ),
-        AutoMigration(
-            from = 3,
-            to = 4,
-        ),
-    ],
+    version = 1,
+    exportSchema = true
 )
 @TypeConverters(DateTypeConverter::class, DayOfWeekTypeConverter::class)
 abstract class BookYouuDataBase : RoomDatabase() {
